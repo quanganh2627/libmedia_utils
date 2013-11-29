8756e6d9e439757bf64df713363e455dd4f0d655
Yu, Linda <linda.yu@intel.com>
2013-11-30 05:04:21 +0800

    Do 2X FRC for 25fps input
    BZ: 153934
    
    Do 2X FRC for 25fps input
    
    Change-Id: I37dd40795b994b73dd25979ff265524250b4d5d1
    Signed-off-by: Yu, Linda <linda.yu@intel.com>

--- VPP/VPPWorker.cpp
+++ VPP/VPPWorker.cpp
@@ -421,11 +421,11 @@ status_t VPPWorker::configFilters(const uint32_t width, const uint32_t height, c
             mFrcOn = true;
             mFrcRate = FRC_RATE_4X;
         }
-        else if (mInputFps == 24 || mInputFps == 25) {
+        else if (mInputFps == 24) {
             mFrcOn = true;
             mFrcRate = FRC_RATE_2_5X;
         }
-        else if (mInputFps == 30) {
+        else if (mInputFps == 30 || mInputFps == 25) {
             mFrcOn = true;
             mFrcRate = FRC_RATE_2X;
         }
